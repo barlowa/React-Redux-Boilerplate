301ec827d4ecc4be65f2f772b98f36a011c53710
alex <abarlow90@gmail.com>
2018-07-12 20:59:30 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+//Redux 
+import App from './pages/App'
+import { Provider } from 'react-redux'
+//CSS
+import './css/App.css'
+import { PersistGate } from 'redux-persist/integration/react'
+import {store, persistor} from './store'
+//react router
+
+//main render 
+ReactDOM.render(
+  <Provider store = {store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root'),
+);
+registerServiceWorker();
